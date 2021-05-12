87ee6393825bcf91f1d2729f3dbad25172f237c1
Elmer Oswaldo Romero Linares <>
2021-05-12 09:59:09 -0600

    main method implementation, String file to store strings for outputs, phase 1 and phase 2 text files

---
+++ src/Main.kt
@@ -0,0 +1,26 @@
+import java.text.MessageFormat
+
+const val cargoPhase1File = "phase1.txt"
+const val cargoPhase2File = "phase2.txt"
+
+fun main() {
+    runMarsSimulation()
+}
+
+fun runMarsSimulation() {
+    val marsSimulation = Simulation()
+
+    val phase1RocketsU1 = marsSimulation.loadU1(cargoPhase1File)
+    val phase1RocketsU2 = marsSimulation.loadU2(cargoPhase1File)
+    val phase2RocketsU1 = marsSimulation.loadU1(cargoPhase2File)
+    val phase2RocketsU2 = marsSimulation.loadU2(cargoPhase2File)
+
+    val totalCostU1Rockets =
+        marsSimulation.runSimulation(phase1RocketsU1) + marsSimulation.runSimulation(phase2RocketsU1)
+
+    val totalCostU2Rockets =
+        marsSimulation.runSimulation(phase1RocketsU2) + marsSimulation.runSimulation(phase2RocketsU2)
+
+    println(MessageFormat.format(u1TotalCost, totalCostU1Rockets.toString()))
+    println(MessageFormat.format(u2TotalCost, totalCostU2Rockets.toString()))
+}
